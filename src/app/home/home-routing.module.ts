3ba537889693930efc25c7710242de99a9ee6eac
Pedro Henrique Vieira Ribeiro <pedrohvribeiro@gmail.com>
2022-12-01 00:28:58 -0300

    Add: validações

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-import { RegistroPage } from './registro.page';
+import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: RegistroPage
+    component: HomePage
   }
 ];
 
@@ -15,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class RegistroPageRoutingModule {}
+export class HomePageRoutingModule {}
